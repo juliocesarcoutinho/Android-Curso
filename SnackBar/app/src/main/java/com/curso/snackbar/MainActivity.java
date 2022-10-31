c3b0848a42095131be01cd2ab5ff31c469d0a963
Julio Cesar Coutinho <juliocesar.coutinho@outlook.com>
2022-10-31 11:04:11 -0300

    SnackBar

---
+++ SnackBar/app/src/main/java/com/curso/snackbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.curso.snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonAbrir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonAbrir = findViewById(R.id.buttonAbrir);
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+
+
+            }
+        });
+
+    }
+}
